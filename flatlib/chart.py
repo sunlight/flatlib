e56a2946f85c63b05113510a4e06a70bd85aaaaa
Robert Davies <robert@theriftlab.com>
2020-12-13 20:31:06 -0800

    Added new functionality to Chart and Datetime clases.

--- flatlib/chart.py
+++ flatlib/chart.py
@@ -1,23 +1,23 @@
 """
     This file is part of flatlib - (C) FlatAngle
     Author: João Ventura (flatangleweb@gmail.com)
-    
 
-    This module implements a class to represent an 
+
+    This module implements a class to represent an
     astrology Chart. It provides methods to handle
     the chart, as well as three relevant properties:
-    
+
     - objects: a list with the chart's objects
     - houses: a list with the chart's houses
     - angles: a list with the chart's angles
 
     Since houses 1 and 10 may not match the Asc and
-    MC in some house systems, the Chart class 
+    MC in some house systems, the Chart class
     includes the list of angles. The angles should be
     used when you want to deal with angle's longitudes.
-    
+
     There are also methods to access fixed stars.
-    
+
 """
 
 from . import angle
@@ -25,6 +25,8 @@ from . import const
 from . import utils
 from .ephem import ephem
 from .datetime import Datetime
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 
 # ------------------ #
@@ -33,26 +35,26 @@ from .datetime import Datetime
 
 class Chart:
     """ This class represents an astrology chart. """
-    
+
     def __init__(self, date, pos, **kwargs):
         """ Creates an astrology chart for a given
-        date and location. 
-        
+        date and location.
+
         Optional arguments are:
         - hsys: house system
         - IDs: list of objects to include
-        
+
         """
         # Handle optional arguments
         hsys = kwargs.get('hsys', const.HOUSES_DEFAULT)
         IDs = kwargs.get('IDs', const.LIST_OBJECTS_TRADITIONAL)
-        
+
         self.date = date
         self.pos = pos
         self.hsys = hsys
         self.objects = ephem.getObjectList(IDs, date, pos)
         self.houses, self.angles = ephem.getHouses(date, pos, hsys)
-        
+
     def copy(self):
         """ Returns a deep copy of this chart. """
         chart = Chart.__new__(Chart)
@@ -63,26 +65,26 @@ class Chart:
         chart.houses = self.houses.copy()
         chart.angles = self.angles.copy()
         return chart
-    
-    
+
+
     # === Properties === #
-    
+
     def getObject(self, ID):
         """ Returns an object from the chart. """
         return self.objects.get(ID)
-    
+
     def getHouse(self, ID):
         """ Returns an house from the chart. """
         return self.houses.get(ID)
-    
+
     def getAngle(self, ID):
         """ Returns an angle from the chart. """
         return self.angles.get(ID)
-    
+
     def get(self, ID):
-        """ Returns an object, house or angle 
+        """ Returns an object, house or angle
         from the chart.
-        
+
         """
         if ID.startswith('House'):
             return self.getHouse(ID)
@@ -93,15 +95,15 @@ class Chart:
 
 
     # === Fixed stars === #
-    
+
     # The computation of fixed stars is inefficient,
     # so the access must be made directly to the
     # ephemeris only when needed.
-        
+
     def getFixedStar(self, ID):
         """ Returns a fixed star from the ephemeris. """
         return ephem.getFixedStar(ID, self.date)
-    
+
     def getFixedStars(self):
         """ Returns a list with all fixed stars. """
         IDs = const.LIST_FIXED_STARS
@@ -109,14 +111,14 @@ class Chart:
 
 
     # === Houses and angles === #
-    
+
     def isHouse1Asc(self):
         """ Returns true if House1 is the same as the Asc. """
         house1 = self.getHouse(const.HOUSE1)
         asc = self.getAngle(const.ASC)
         dist = angle.closestdistance(house1.lon, asc.lon)
         return abs(dist) < 0.0003  # 1 arc-second
-    
+
     def isHouse10MC(self):
         """ Returns true if House10 is the same as the MC. """
         house10 = self.getHouse(const.HOUSE10)
@@ -126,19 +128,19 @@ class Chart:
 
 
     # === Other properties === #
-    
+
     def isDiurnal(self):
         """ Returns true if this chart is diurnal. """
         sun = self.getObject(const.SUN)
         mc = self.getAngle(const.MC)
-        
+
         # Get ecliptical positions and check if the
         # sun is above the horizon.
         lat = self.pos.lat
         sunRA, sunDecl = utils.eqCoords(sun.lon, sun.lat)
         mcRA, mcDecl = utils.eqCoords(mc.lon, 0)
         return utils.isAboveHorizon(sunRA, sunDecl, mcRA, lat)
-    
+
     def getMoonPhase(self):
         """ Returns the phase of the moon. """
         sun = self.getObject(const.SUN)
@@ -152,18 +154,66 @@ class Chart:
             return const.MOON_THIRD_QUARTER
         else:
             return const.MOON_LAST_QUARTER
-    
-    
+
+
     # === Solar returns === #
-    
+
     def solarReturn(self, year):
-        """ Returns this chart's solar return for a 
-        given year. 
-        
+        """ Returns this chart's solar return for a
+        given year.
+
         """
         sun = self.getObject(const.SUN)
-        date = Datetime('{0}/01/01'.format(year),
+        date = Datetime('{0}-01-01'.format(year),
                         '00:00',
                         self.date.utcoffset)
         srDate = ephem.nextSolarReturn(date, sun.lon)
         return Chart(srDate, self.pos, hsys=self.hsys)
+
+
+    # === Progressions === #
+
+    def progressedChart(self, date):
+        """ Return the progressed chart for a
+        given date.
+
+        """
+        day_length = 23.9344    # 23 hours 56 minutes 4 seconds
+        tz = self.date.getTz(self.pos)
+        progression_date = tz.localize(datetime.fromisoformat(date))
+        chart_date = self.date.datetime
+        start_date = tz.localize(chart_date)
+        last_date = start_date.replace(year=progression_date.year)
+
+        # the progression year is counted from birthday to birthday
+        # this calculates how far along in the year the progression date is
+        if (last_date > progression_date):
+            next_date = last_date
+            last_date -= relativedelta(years=1)
+        else:
+            next_date = last_date + relativedelta(years=1)
+
+        year_length = (next_date - last_date).days * day_length
+        days_passed = (progression_date - last_date).days * day_length
+        days_passed += relativedelta(progression_date, last_date).hours
+        days_passed_ratio = days_passed / year_length
+
+        # calculate progression chart date / time
+        days = relativedelta(progression_date, start_date).years
+        hours = days_passed_ratio * day_length
+        pcd = chart_date + relativedelta(days=days, hours=hours)
+        progressed_chart_date = Datetime([pcd.year, pcd.month, pcd.day], ['+', pcd.hour, pcd.minute, pcd.second], self.date.utcoffset)
+        return Chart(progressed_chart_date, self.pos, hsys=self.hsys)
+
+
+    # === Transits === #
+
+    def currentTransits(self):
+        """ Returns the current moment's chart.
+
+        """
+        now = datetime.now()
+        date = now.strftime("%Y-%m-%d")
+        time = now.strftime("%H:%M:%S")
+        transit_date = Datetime(date, time, self.date.utcoffset)
+        return Chart(transit_date, self.pos, hsys=self.hsys)
--- flatlib/datetime.py
+++ flatlib/datetime.py
@@ -1,20 +1,25 @@
 """
     This file is part of flatlib - (C) FlatAngle
     Author: João Ventura (flatangleweb@gmail.com)
-    
 
-    This module provides functions and classes for handling 
+
+    This module provides functions and classes for handling
     dates and times.
-    
-    The classes implemented in this file are <Date>, <Time> 
-    and <Datetime>. Since time is similar to angles (same 
-    string separators and base 60), the <Time> class uses 
+
+    The classes implemented in this file are <Date>, <Time>
+    and <Datetime>. Since time is similar to angles (same
+    string separators and base 60), the <Time> class uses
     angular functions for internal conversions.
 
 """
 
 from . import angle
-
+from .geopos import GeoPos
+from datetime import datetime
+from pytz import timezone, utc
+from timezonefinder import TimezoneFinder
+from backports.datetime_fromisoformat import MonkeyPatch
+MonkeyPatch.patch_fromisoformat()
 
 # Calendar types
 GREGORIAN = 0
@@ -32,7 +37,7 @@ def dateJDN(year, month, day, calendar):
         return day + (153*m + 2)//5 + 365*y + y//4 - y//100 + y//400 - 32045
     else:
         return day + (153*m + 2)//5 + 365*y + y//4 - 32083
-    
+
 def jdnDate(jdn):
     """ Converts Julian Day Number to Gregorian date. """
     a = jdn + 32044
@@ -54,21 +59,21 @@ def jdnDate(jdn):
 class Date:
     """ This class represents a calendar date. It is
     internally represented by a JDN integer.
-    
-    Objects of this class can be instantiated with 
+
+    Objects of this class can be instantiated with
     dates of type string, list and int (jdn).
-    String and date lists are like 'yyyy/mm/dd'.
-    
+    String and date lists are like 'yyyy-mm-dd'.
+
     """
-    
+
     # Calendar types
     GREGORIAN = GREGORIAN
     JULIAN = JULIAN
-    
+
     def __init__(self, value, calendar=GREGORIAN):
         if isinstance(value, str):
-            # Assume string date such as "2015/03/29"
-            value = [int(v) for v in value.split('/')]
+            # Assume string date such as "2015-03-29"
+            value = [int(v) for v in value.split('-')]
             value = dateJDN(value[0], value[1], value[2], calendar)
         elif isinstance(value, list):
             # Assume list date such as [2015,03,29]
@@ -78,28 +83,28 @@ class Date:
     def dayofweek(self):
         """ Returns the day of week starting on Sunday as zero. """
         return (self.jdn + 1) % 7
-    
+
     def date(self):
         """ Returns date as list [yyyy,mm,dd]. """
         return jdnDate(self.jdn)
-    
+
     def toList(self):
         """ Returns date as signed list. """
         date = self.date()
         sign = '+' if date[0] >= 0 else '-'
         date[0] = abs(date[0])
         return list(sign) + date
-    
+
     def toString(self):
         """ Returns date as string. """
         slist = self.toList()
         sign = '' if slist[0] == '+' else '-'
-        string = '/'.join(['%02d' % v for v in slist[1:]])
+        string = '-'.join(['%02d' % v for v in slist[1:]])
         return sign + string
-    
+
     def __str__(self):
         return '<%s>' % self.toString()
-    
+
 
 # ------------------ #
 #     Time Class     #
@@ -108,48 +113,48 @@ class Date:
 class Time:
     """ This class represents a time in the library.
     A time from this class can have negative values.
-    
+
     Objects of this class can be instantiated with
     strings, signed lists or float values.
     String and time lists are like 'hh:mm:ss.'
-    
+
     """
-    
+
     def __init__(self, value):
         self.value = angle.toFloat(value)
-        
+
     def getUTC(self, utcoffset):
-        """ Returns a new Time object set to UTC given 
+        """ Returns a new Time object set to UTC given
         an offset Time object.
-        
+
         """
         newTime = (self.value - utcoffset.value) % 24
         return Time(newTime)
-    
+
     def time(self):
         """ Returns time as list [hh,mm,ss]. """
         slist = self.toList()
         if slist[0] == '-':
             slist[1] *= -1
-            # We must do a trick if we want to 
+            # We must do a trick if we want to
             # make negative zeros explicit
             if slist[1] == -0:
                 slist[1] = -0.0
         return slist[1:]
-    
+
     def toList(self):
         """ Returns time as signed list. """
         slist = angle.toList(self.value)
         # Keep hours in 0..23
         slist[1] = slist[1] % 24
         return slist
-    
+
     def toString(self):
         """ Returns time as string. """
         slist = self.toList()
         string = angle.slistStr(slist)
         return string if slist[0] == '-' else string[1:]
-    
+
     def __str__(self):
         return '<%s>' % self.toString()
 
@@ -162,30 +167,34 @@ class Datetime:
     """ This class represents a specific moment in time given by
     a date, a time and an UTC Offset. The UTC Offset is zero
     by default (UTC+0) although an offset can be given.
-    
+
     """
-    
+
     # Calendar types
     GREGORIAN = GREGORIAN
     JULIAN = JULIAN
-    
-    def __init__(self, date, time=0, utcoffset=0, calendar=GREGORIAN):
+
+    def __init__(self, date, time=0, utcoffset=0, calendar=GREGORIAN, pos=0):
         # Prepare the variables
         if isinstance(date, Date):
             self.date = date
         else:
             self.date = Date(date, calendar)
-            
+
         if isinstance(time, Time):
             self.time = time
         else:
             self.time = Time(time)
-            
+
         if isinstance(utcoffset, Time):
             self.utcoffset = utcoffset
+        elif isinstance(pos, GeoPos):
+            self.utcoffset = Time(self.getOffset(pos))
         else:
             self.utcoffset = Time(utcoffset)
-            
+
+        self.datetime = datetime.fromisoformat(self.toString())
+
         # Compute jd
         self.jd = self.date.jdn + self.time.value/24.0 - \
                   self.utcoffset.value/24.0 - 0.5
@@ -200,13 +209,28 @@ class Datetime:
         time = Time((localJD + 0.5 - date.jdn) * 24)
         return Datetime(date, time, utcoffset)
 
+    def getTz(self, pos):
+        """ Returns the timezone for a location. """
+        return timezone(TimezoneFinder().certain_timezone_at(lng=pos.lon, lat=pos.lat))
+
+    def getOffset(self, pos):
+        """ Calculates a timezone offset based on position. """
+        tz = self.getTz(pos)
+        date = datetime.fromisoformat(self.date.toString() + ' ' + self.time.toString())
+        date_local = tz.localize(date)
+        date_utc = utc.localize(date)
+        return int((date_utc - date_local).total_seconds() / 3600)
+
     def getUTC(self):
         """ Returns this Datetime localized for UTC. """
         timeUTC = self.time.getUTC(self.utcoffset)
         dateUTC = Date(round(self.jd))
         return Datetime(dateUTC, timeUTC)
-    
+
+    def toString(self):
+        return self.date.toString() + ' ' + self.time.toString()
+
     def __str__(self):
         return '<%s %s %s>' % (self.date.toString(),
                                self.time.toString(),
-                               self.utcoffset.toString()) 
+                               self.utcoffset.toString())
